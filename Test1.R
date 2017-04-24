604557d77f59cb999b57cbdcdca751207eca67bd
paulranum11 <paulranum11@gmail.com>
2017-04-24 12:00:55 -0500

    I cant seem to see this script on github

---
+++ Test1.R
@@ -0,0 +1,10 @@
+# This is to test adding a new script to github
+
+Data1 <- c(1,2,3,4,5,6,7,8,9)
+Data2 <- c(10,100,10,100,10,100,10,100,10)
+
+DF <- data.frame(Data1, Data2)
+
+
+# I cant seem to see this script on GitHub
+ 
